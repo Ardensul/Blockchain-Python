a79870c6bdc3f7993d007bfc57257ff5a89a08bc
Quentin <qthomas67150@gmail.com>
2019-05-20 17:43:50 +0200

    Update api/models

--- django/wallet/api/models.py
+++ django/wallet/api/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 class Transaction(models.Model):
     date = models.DateField("date")
-    sender = models.CharField("sender")
-    receive = models.CharField("receive")
+    sender = models.TextField("sender")
+    receive = models.TextField("receive")
     amount = models.IntegerField("amount")
+
+class User(models.Model):
+    private_key = models.TextField("private_key")
+    public_key = models.TextField("public_key")
+    balance = models.FloatField("balance")
