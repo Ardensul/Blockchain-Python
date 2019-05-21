f24b65532bc7e180f7bf4e412a250b84ea2e40bc
Ardensul <romainscarato@gmail.com>
2019-05-21 11:56:03 +0200

    Ajout du chargement et sauvegarde de la blockchain dans le fichier json et ajout de la fonction verifiant l'integrité de la blockchain

--- blochain/Block.py
+++ blochain/Block.py
@@ -1,25 +1,33 @@
 from hashlib import *
 
+
 class Block:
-    def __init__(self, data, prevhash = 'genesis'):
+    def __init__(self, data, prevhash = 'genesis',id = 0,workProof=0,currentHash=''):
+        self.id = id
         self.previousHash = prevhash
-        # self.emeteur = data["emeteur"]
-        # self.receveur = data["receveur"]
-        # self.montant = data["montant"]
         self.data = data
-        self.signature = "bite"
-        self.workProof = 0
-        self.currentHash = self.currenthash()
+        self.workProof = workProof
+        if currentHash != '':
+            self.currentHash = currentHash
+        else:
+            self.currentHash = self.currenthash()
 
     def currenthash(self):
         isfinish = False
         hash_objet = ''
         while not isfinish :
-            block = str(self.previousHash) + str(self.data) + str(self.signature) + str(self.workProof)
+            block = str(self.previousHash) + str(self.data) + str(self.workProof)
             block = block.encode(encoding='Utf-8')
+            hash_objet = sha256(block).hexdigest()
             if hash_objet[0:5] != "0000a":
                 self.workProof += 1
             else:
                 isfinish = True
-                print("Bien évidemment la solution était : " + hash_objet)
+        return hash_objet
+
+
+    def verifyhash(self):
+        block = str(self.previousHash) + str(self.data) + str(self.workProof)
+        block = block.encode(encoding='Utf-8')
+        hash_objet = sha256(block).hexdigest()
         return hash_objet
--- blochain/Chain.py
+++ blochain/Chain.py
@@ -1,18 +1,23 @@
 from Block import *
 import rsa
+import os
+import json
 
 class Chain:
     def __init__(self):
-        self.blockChain = [1]
-        if len(self.blockChain) == 1:
-            self.blockChain[0] = Block('genesis')
-
-    def addBlock(self,block):
-        self.blockChain.append(block)
+        self.path = "../test/chain.json"
+        self.blockChain = [0]
+        exist = os.path.isfile(self.path)
+        if exist:
+            self.load_chain()
+        else:
+            if len(self.blockChain) == 1:
+                self.blockChain[0] = Block("genesis")
 
     def generateBlock(self,data):
-        block = Block(data,self.blockChain[-1].currentHash)
+        block = Block(data,self.blockChain[-1].currentHash,self.blockChain[-1].id+1)
         self.blockChain.append(block)
+        self.saveChain()
 
     def data_check(self,data):
         if not data.emeteur or not data.receveur or not data.montant or not data.publicKey or not data.signature:
@@ -23,12 +28,28 @@ class Chain:
             return False
         return True
 
+    def integrity_check(self):
+        for i in range(1,len(self.blockChain)):
+            if (self.blockChain[i].previousHash == self.blockChain[i-1].verifyhash()) and (self.blockChain[i].verifyhash() == self.blockChain[i].currentHash) :
+                continue
+            else:
+                return False
+        return True
 
-test = Chain()
+    def load_chain(self):
+        saveFile = open(self.path,'r')
+        data = json.load(saveFile)
+        for i in range(len(data["block"])):
+            if i == 0:
+                self.blockChain[i] = Block(data["block"][i]["data"],data["block"][i]["previousHash"],data["block"][i]["id"],data["block"][i]["workProof"],data["block"][i]["currentHash"])
+            else:
+                self.blockChain.append(Block(data["block"][i]["data"],data["block"][i]["previousHash"],data["block"][i]["id"],data["block"][i]["workProof"],data["block"][i]["currentHash"]))
 
-for i in range(5):
-    test.generateBlock(i)
-
-for i in range(len(test.blockChain)):
-    print(test.blockChain[i].__dict__)
-
+    def saveChain(self):
+        saveFile = open(self.path,'w')
+        data = {}
+        data["block"] = []
+        for i in range(len(self.blockChain)):
+            data["block"].append(self.blockChain[i].__dict__)
+        json.dump(data,saveFile)
+        return True
