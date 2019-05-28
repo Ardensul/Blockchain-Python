386b967973f7b762daa58d522c0e05aaf44a2bbd
Alban Durrheimer <alban6800@hotmail.fr>
2019-05-28 08:18:11 +0200

    Update api model

--- django/wallet/api/models.py
+++ django/wallet/api/models.py
@@ -1,15 +1,28 @@
+import rsa
 from django import forms
 
 
 class User(forms.Form):
-    private_key = forms.CharField(required=True)
-    public_key = forms.CharField(required=True)
+    private_key = forms.CharField(required=True, widget=forms.Textarea)
+    public_key = forms.CharField(required=True, widget=forms.Textarea)
+
+    def get_private_key(self):
+        return rsa.PrivateKey.load_pkcs1(self.private_key)  # FIXME
+
+    def get_public_key(self):
+        return rsa.PublicKey.load_pkcs1(self.public_key)  # FIXME
 
     def get_unique_key(self):
         return self["public_key"]  # TODO
 
     def get_amount(self):
         return self["public_key"]  # TODO
+
+    @staticmethod
+    def create_new_user():
+        (public, private) = rsa.newkeys(512)
+        return {"public": public.save_pkcs1().decode('ascii'),
+                "private": private.save_pkcs1().decode('ascii')}
 
 
 class Transaction(forms.Form):
