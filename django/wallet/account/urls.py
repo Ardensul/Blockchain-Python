e445c5bc915609ddf24b3f66afe515b366330cfa
Alban Durrheimer <alban6800@hotmail.fr>
2019-05-30 12:48:30 +0200

    Add POST request for transaction and update template

--- django/wallet/account/urls.py
+++ django/wallet/account/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.transaction, name='index'),
+    path("", views.index, name='index'),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
-    path("transaction/", views.transaction, name="transaction")
+    path("transaction/", views.transaction, name="transaction"),
+    path("transaction/send", views.send_transaction, name="send_transaction"),
+    path("transaction/payment", views.send_payment, name="send_payment")
 ]
--- django/wallet/account/views.py
+++ django/wallet/account/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render, redirect
 from account.models import Transaction, User, PayingCard
 
 
+def index(request):
+    return render(request, 'form.html', {"login": True})
+
+
 def login(request):
     form = User(request.POST or None)
     if form.is_valid():
@@ -21,6 +25,30 @@ def logout(request):
 
 def transaction(request):
     transaction_form = Transaction(None)
-    card_form = PayingCard(request.POST or None)
-    return render(request, 'transaction.html',
-                  {"login": True, "transaction_form": transaction_form, "card_form": card_form})
+    card_form = PayingCard(None)
+
+    # noinspection PyShadowingNames
+    login = True
+    return render(request, 'transaction.html', locals())
+
+
+def send_transaction(request):
+    if request.method == "POST":
+        transaction_form = Transaction(request.POST)
+
+        if transaction_form.is_valid():  # TODO: send transaction to miner
+            user = User(request.session["user"])
+            print()
+    else:
+        return redirect("transaction")
+
+
+def send_payment(request):
+    if request.method == "POST":
+        payment_form = PayingCard(request.POST)
+
+        if payment_form.is_valid():  # TODO: create payment
+            user = User(request.session["user"])
+            print()
+    else:
+        return redirect("transaction")
