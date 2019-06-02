bb254d986d77d2a13e9d862c4415d6db8ed04906
Alban Durrheimer <alban6800@hotmail.fr>
2019-06-03 00:48:14 +0200

    Added docstring to django/server

--- django/server/api/models.py
+++ django/server/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Address(models.Model):
+    """Model representing the Address table in the database"""
     address = models.TextField()
 
     def __str__(self):
--- django/server/api/views.py
+++ django/server/api/views.py
@@ -9,6 +9,13 @@ logger = logging.getLogger(__name__)
 
 
 def index(request):
+    """Processes requests on the API.
+    If it is a POST request, it will add the address contained in parameter in its list.
+    If it is a DELETE request, it will delete the address contained in the parameter from its list.
+
+    :param request: Object containing the web request
+    :return: a JSON response containing the available address list
+    """
     if request.method == "POST":
         body_unicode = request.body.decode('utf-8')
         body_data = json.loads(body_unicode)
